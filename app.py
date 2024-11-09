b9f4a893d6ec8fc597cfbe7847237f927a5af29e
Punya Galundia <spypunya@gmail.com>
2024-11-09 00:39:44 -0500

    login-signup link

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route("/signUp")
+def signUp():
+    return render_template('signUp.html')
+
 if __name__ == '__main__':
    app.run(debug=True)
